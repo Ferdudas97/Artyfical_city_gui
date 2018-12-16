d6f63f4b6dcc83684de99757f7cd3cdf80256507
ferdudas97 <ferdudas97@gmail.com>
2018-12-16 20:58:13 +0100

    algorithm is working

--- src/main/java/sample/service/CanvasService.kt
+++ src/main/java/sample/service/CanvasService.kt
@@ -36,13 +36,22 @@ class CanvasService(val gc: GraphicsContext, val nodeSize: Double) {
         carMap.putIfAbsent(id, pickRandomColor())
         gc.fill = carMap[id]
         val delta = carDeltaInSpecifiedDirection(head.direction, size)
-        gc.fillRect(head.horizontalPosition, head.verticalPosition, delta.first, delta.second)
+        drawCarInSpecifiedDirection(head.horizontalPosition, head.verticalPosition,head.direction, delta)
 //        gc.fillRect(head.horizontalPosition+nodeSize/4, head.verticalPosition+nodeSize/4, delta.first, delta.second)
     }
 
-    fun carDeltaInSpecifiedDirection(nodeDirection: NodeDirection, size: Int) = when(nodeDirection) {
-        NodeDirection.DOWN, NodeDirection.UP -> (nodeSize) to (nodeSize)*size
-        NodeDirection.LEFT ,NodeDirection.RIGHT -> (nodeSize)*size to (nodeSize)
+    fun drawCarInSpecifiedDirection(x: Double, y: Double, direction: NodeDirection, delta: Pair<Double, Double>) = when (direction) {
+        NodeDirection.LEFT -> gc.fillRect(x, y, delta.first, delta.second)
+        NodeDirection.RIGHT -> gc.fillRect(x - nodeSize, y, delta.first, delta.second)
+        NodeDirection.UP -> gc.fillRect(x, y, delta.first, delta.second)
+        NodeDirection.DOWN -> gc.fillRect(x , y-nodeSize, delta.first, delta.second)
+
+
+    }
+
+    fun carDeltaInSpecifiedDirection(nodeDirection: NodeDirection, size: Int) = when (nodeDirection) {
+        NodeDirection.DOWN, NodeDirection.UP -> (nodeSize) to (nodeSize) * size
+        NodeDirection.LEFT, NodeDirection.RIGHT -> (nodeSize) * size to (nodeSize)
     }
 
 
